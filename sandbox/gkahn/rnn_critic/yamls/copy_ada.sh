528e4a2b74957d49e85cd5daf23b139ce4661546
Gregory Kahn <gkahn13@gmail.com>
2017-05-13 20:46:51 -0700

    no yamls

--- sandbox/gkahn/rnn_critic/yamls/copy_ada.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-scp $1 gkahn@ada.ist.berkeley.edu:/home/gkahn/code/rllab/sandbox/gkahn/rnn_critic/examples/yamls
--- sandbox/gkahn/rnn_critic/yamls/copy_newton1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-scp $1 gkahn@newton1.banatao.berkeley.edu:/home/gkahn/code/rllab/sandbox/gkahn/rnn_critic/examples/yamls
