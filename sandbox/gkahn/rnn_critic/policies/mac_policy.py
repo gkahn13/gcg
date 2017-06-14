hopefully fixed gradient size bug

sandbox/gkahn/rnn_critic/policies/mac_policy.py
@@ -669,40 +669,83 @@ class MACPolicy(TfPolicy, Serializable):
         tf.assert_equal(tf.reduce_sum(tf_weights, 0), 1.)
         tf.assert_equal(tf.reduce_sum(tf_train_values_softmax, 1), 1.)
 
+        ############################################
+        ############### DEBUG ######################
+        ############################################
+
+        ### separate mses
+        # tf_mses = []
+        # for n in range(self._N):
+        #     tf_sum_rewards_n = tf.reduce_sum(np.power(self._gamma * np.ones(n + 1), np.arange(n + 1)) *
+        #                                      tf_rewards_ph[:, :n + 1],
+        #                                      reduction_indices=1)
+        #     tf_target_values_n = (1 - tf_dones[:, n]) * np.power(self._gamma, n + 1) * tf_target_get_action_values[:, n]
+        #     if self._separate_target_params:
+        #         tf_target_values_n = tf.stop_gradient(tf_target_values_n)
+        #     tf_train_values_n = tf_train_values[:, n]
+        #
+        #     tf_mses.append(tf.square(tf_sum_rewards_n + tf_target_values_n - tf_train_values_n))
+        #
+        # tf_mse_separate = tf.reduce_sum(tf_weights * tf_train_values_softmax * tf.pack(tf_mses, 1))
+
+        ### one mse
+        tf_values_desired = []
+        for n in range(self._N):
+            tf_sum_rewards_n = tf.reduce_sum(np.power(self._gamma * np.ones(n + 1), np.arange(n + 1)) *
+                                             tf_rewards_ph[:, :n + 1],
+                                             reduction_indices=1)
+            tf_target_values_n = (1 - tf_dones[:, n]) * np.power(self._gamma, n + 1) * tf_target_get_action_values[:, n]
+            if self._separate_target_params:
+                tf_target_values_n = tf.stop_gradient(tf_target_values_n)
+            tf_values_desired.append(tf_sum_rewards_n + tf_target_values_n)
+        tf_values_desired = tf.pack(tf_values_desired, 1)
+
+        tf_mse_separate = tf.reduce_sum(tf_weights * tf_train_values_softmax *
+                                        tf.square(tf_train_values - tf_values_desired))
+
+        tf_mse_one = tf.reduce_sum(tf_weights[:, 0] *
+                                   tf.reduce_mean(
+                                       tf.square(tf_train_values -
+                                                 tf.tile(
+                                                     tf.reduce_sum(tf_train_values_softmax * tf_values_desired, axis=1, keep_dims=True),
+                                                 (1, self._N))),
+                                       1))
+
+
+        self._tf_debug['tf_mse_separate'] = tf_mse_separate
+        self._tf_debug['tf_mse_one'] = tf_mse_one
+        self._tf_debug['tf_weights'] = tf_weights
+        self._tf_debug['tf_softmax'] = tf_train_values_softmax
+        self._tf_debug['tf_train_values'] = tf_train_values
+
+        import IPython; IPython.embed()
+
+
+        ############################################
+        ############### END DEBUG ##################
+        ############################################
+
+        tf_values_desired = []
+        for n in range(self._N):
+            tf_sum_rewards_n = tf.reduce_sum(np.power(self._gamma * np.ones(n + 1), np.arange(n + 1)) *
+                                             tf_rewards_ph[:, :n + 1],
+                                             reduction_indices=1)
+            tf_target_values_n = (1 - tf_dones[:, n]) * np.power(self._gamma, n + 1) * tf_target_get_action_values[:, n]
+            if self._separate_target_params:
+                tf_target_values_n = tf.stop_gradient(tf_target_values_n)
+            tf_values_desired.append(tf_sum_rewards_n + tf_target_values_n)
+        tf_values_desired = tf.pack(tf_values_desired, 1)
+
         if self._separate_mses:
-            tf_mses = []
-            for n in range(self._N):
-                tf_sum_rewards_n = tf.reduce_sum(np.power(self._gamma * np.ones(n+1), np.arange(n+1)) *
-                                                 tf_rewards_ph[:,:n+1],
-                                                 reduction_indices=1)
-                tf_target_values_n = (1 - tf_dones[:,n]) * np.power(self._gamma, n+1) * tf_target_get_action_values[:,n]
-                if self._separate_target_params:
-                    tf_target_values_n = tf.stop_gradient(tf_target_values_n)
-                tf_train_values_n = tf_train_values[:, n]
-
-                tf_mses.append(tf.square(tf_sum_rewards_n + tf_target_values_n - tf_train_values_n))
-
-            # tf_train_values_softmax is across multiple N-step returns (columns)
-            # tf_weights is across the batch (rows)
-            tf_mse = tf.reduce_sum(tf_weights * tf_train_values_softmax * tf.pack(tf_mses, 1))
+            tf_mse = tf.reduce_sum(tf_weights * tf_train_values_softmax * tf.square(tf_train_values - tf_values_desired))
         else:
-            ### calculate all n-step values
-            tf_values_desired = []
-            for n in range(self._N):
-                tf_sum_rewards_n = tf.reduce_sum(np.power(self._gamma * np.ones(n+1), np.arange(n+1)) *
-                                                 tf_rewards_ph[:,:n+1],
-                                                 reduction_indices=1)
-                tf_target_values_n = (1 - tf_dones[:,n]) * np.power(self._gamma, n+1) * tf_target_get_action_values[:,n]
-                if self._separate_target_params:
-                    tf_target_values_n = tf.stop_gradient(tf_target_values_n)
-                tf_values_desired.append(tf_sum_rewards_n + tf_target_values_n)
-            tf_values_desired = tf.pack(tf_values_desired, 1)
-
-            ### form final mse
-            # tf_train_values_softmax is across multiple N-step returns (columns)
-            # tf_weights is across the batch (rows)
-            tf_mse = tf.reduce_sum(tf_weights * tf.square(tf_train_values_softmax *
-                                                         (tf_values_desired - tf_train_values)))
+            tf_mse = tf.reduce_sum(tf_weights[:, 0] *
+                                   tf.reduce_mean(
+                                       tf.square(tf_train_values -
+                                                 tf.tile(
+                                                     tf.reduce_sum(tf_train_values_softmax * tf_values_desired, axis=1, keep_dims=True),
+                                                     (1, self._N))),
+                                       1))
 
 
         ### weight decay
@@ -817,6 +860,18 @@ class MACPolicy(TfPolicy, Serializable):
                                                tf_target_get_action_values, tf_logprob_curr, tf_logprob_prior_ph)
             tf_opt, tf_lr_ph = self._graph_optimize(tf_cost, tf_trainable_policy_vars)
 
+
+            ##############################
+            ########## DEBUG #############
+            ##############################
+
+            self._tf_debug['tf_grad_separate'] = tf.gradients(self._tf_debug['tf_mse_separate'], tf_policy_vars[2])
+            self._tf_debug['tf_grad_one'] = tf.gradients(self._tf_debug['tf_mse_one'], tf_policy_vars[2])
+
+            ##############################
+            ########## END DEBUG #########
+            ##############################
+
             ### initialize
             self._graph_init_vars(tf_sess)
 
@@ -907,14 +962,18 @@ class MACPolicy(TfPolicy, Serializable):
         if self._use_target:
             feed_dict[self._tf_dict['obs_target_ph']] = observations
 
-        # if step > 1.1e4:
-        #     d = {}
-        #     keys = [k for k in self._tf_debug.keys()]
-        #     vs = self._tf_dict['sess'].run([self._tf_debug[k] for k in keys], feed_dict=feed_dict)
-        #     for k, v in zip(keys, vs):
-        #         d[k] = v
-        #
-        #     import IPython; IPython.embed()
+        if True: #step > 1.1e4:
+            d = {}
+            keys = [k for k in self._tf_debug.keys()]
+            vs = self._tf_dict['sess'].run([self._tf_debug[k] for k in keys], feed_dict=feed_dict)
+            for k, v in zip(keys, vs):
+                d[k] = v
+
+            gs = d['tf_grad_separate'][0]
+            go = d['tf_grad_one'][0]
+            print('{0}, {1}'.format(np.nanmean(gs / go), np.nanstd(gs / go)))
+
+            import IPython; IPython.embed()
 
         cost, mse, _ = self._tf_dict['sess'].run([self._tf_dict['cost'],
                                                   self._tf_dict['mse'],

sandbox/gkahn/rnn_critic/scripts/plot_imswingup1.py
@@ -27,14 +27,10 @@ def load_experiments(indices):
     return exps
 
 dqn_1 = load_experiments([0, 4, 5])
-dqn_5_et = load_experiments([21, 22, 23])
-dqn_5_wb = load_experiments([6, 7, 8])
-dqn_10_et = load_experiments([27, 28, 29])
-dqn_10_wb = load_experiments([16, 17, 18])
-# rnn_5_wb = load_experiments([9, 10, 11])
-mac_5_et = load_experiments([24, 25, 26])
-mac_5_wb = load_experiments([1, 2, 3])
-mac_10_wb = load_experiments([12, 13, 14])
+dqn_5 = load_experiments([6, 7, 8])
+dqn_10 = load_experiments([16, 17, 18])
+mac_5 = load_experiments([1, 2, 3])
+mac_10 = load_experiments([12, 13, 14])
 
 ############
 ### Plot ###
@@ -79,13 +75,12 @@ def get_threshold(analyze_group, window=200):
 
 import IPython; IPython.embed()
 
-comparison_exps = [dqn_1, dqn_5_et, dqn_5_wb, mac_5_et, mac_5_wb]
-width = 0.4
+comparison_exps = [dqn_1, dqn_5, mac_5, dqn_10, mac_10]
+width = 0.3
 xs = [0,
-      1-1.05*width/2., 1+1.05*width/2.,
-      2-1.05*width/2., 2+1.05*width/2.]
-colors = ['k', 'g', 'g', 'b', 'b']
-patterns = [None, '/', '+', '/', '+']
+      1-0.5*width, 1+0.5*width,
+      2-0.5*width, 2+0.5*width]
+colors = ['k', 'g', 'b', 'g', 'b']
 
 thresh_means, thresh_stds = [], []
 for ag in comparison_exps:
@@ -96,26 +91,28 @@ for ag in comparison_exps:
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=15)
 
-f, ax = plt.subplots(1, 1, figsize=(7, 3))
+f, ax = plt.subplots(1, 1, figsize=(8, 3))
 bars = ax.bar(xs, thresh_means, width=width, yerr=thresh_stds, color=colors,
-              error_kw=dict(ecolor='gray', lw=1, capsize=2, capthick=1))
+              error_kw=dict(ecolor='m', lw=1.5, capsize=4, capthick=1.5))
 
 ax.set_xticks(np.arange(3))
-ax.set_xticklabels([r'Standard Critic', r'MRC', r'MAC (ours)', ''])
+ax.set_xticklabels([r'$N=1$', r'$N=5$', r'$N=10$'])
 ax.set_ylabel('Steps until solved')
 yfmt = ticker.ScalarFormatter()
 yfmt.set_powerlimits((0, 0))
 ax.yaxis.set_major_formatter(yfmt)
 
-for bar, pattern in zip(bars, patterns):
-    bar.set_hatch(pattern)
-
-et_patch = mpatches.Patch(color='k', alpha=0.4, label='ET', hatch='///')
-wb_patch = mpatches.Patch(color='k', alpha=0.4, label='CMR (ours)', hatch='+++')
-ax.legend(handles=(et_patch, wb_patch))
-
 for l in ax.get_xticklabels():
     l.set_multialignment('center')
 
-f.savefig(os.path.join(SAVE_FOLDER, 'imswingup0_comparison.png'), bbox_inches='tight', dpi=200)
+handles = [
+    mpatches.Patch(color='k', label='Double Q-learning'),
+    mpatches.Patch(color='g', label='Multistep Double Q-learning'),
+    mpatches.Patch(color='b', label='MAQL (ours)'),
+]
+
+# ax.legend(handles=handles, loc='upper center', bbox_to_anchor=(1.3, 0.8), ncol=1)
+ax.legend(handles=handles, loc='upper center', bbox_to_anchor=(0.5, 1.45), ncol=2)
+
+f.savefig(os.path.join(SAVE_FOLDER, 'imswingup1_comparison.png'), bbox_inches='tight', dpi=200)
 plt.close(f)